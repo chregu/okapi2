c24ee14acf54be208cdb408620c8bad931487697
lukas.smith <lukas.smith@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2009-05-22 14:58:29 +0000

    updated frontcontroller to use DI
    git-svn-id: https://svn.liip.ch/repos/public/okapi/skel/branches/symfony2@11433 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- www/index.php
+++ www/index.php
@@ -3,5 +3,20 @@
 require_once dirname(__FILE__) . '/../inc/api/init.php';
 $sc = api_init::start();
 
+// get route
+$sc->routingcontainer;
 $ctrl = $sc->controller;
-$ctrl->process();
+$route = $sc->routing->getRoute($sc->request);
+$sc->setService('route', $route);
+
+// get command
+$command = $ctrl->findCommandName($route);
+$command = $sc->$command;
+
+// handle view
+if ($viewName = $ctrl->process($command)) {
+    $view = $sc->$viewName;
+    $view->prepare();
+    $data = $command->getData();
+    $view->dispatch($data, $ctrl->getExceptions());
+}
