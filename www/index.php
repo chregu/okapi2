083bc3ee1d1f3f4901fe007402d85df352674a7a
chregu <chregu@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2009-06-24 08:57:18 +0000

    put $sc into controller, otherwise it gets too messy
    git-svn-id: https://svn.liip.ch/repos/public/okapi/skel/branches/symfony2@11671 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- www/index.php
+++ www/index.php
@@ -2,36 +2,11 @@
 
 require_once dirname(__FILE__) . '/../inc/api/init.php';
 $sc = api_init::start();
-$sc->log;
 // get route
 $sc->routingcontainer;
 $ctrl = $sc->controller;
 
-$ctrl->setScLookup(new ServicesLookup($sc));
+$ctrl->setServiceContainer($sc);
 
 $ctrl->run()->send();
 
-
-class ServicesLookup {
-
-    function __construct($sc) {
-        $this->sc = $sc;
-    }
-
-    function getCommand($command) {
-        return $this->sc->$command;
-}
-
-    function getView($viewName) {
-    try {
-            $view = $this->sc->$viewName;
-    } catch  (InvalidArgumentException $e) {
-            $view = new $viewName($this->sc->route, $this->sc->request, $this->sc->response, $this->sc->config);
-        }
-        return $view;
-    }
-
-    function setRoute($route) {
-        $this->sc->setService('route', $route);
-    }
-}
