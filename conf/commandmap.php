66e57a2bacb762dcea95edc46def2e501f103c3c
jordi.boggiano <jordi.boggiano@99f7f646-bdd5-0310-89f9-ddcb10b10bf3>
2010-02-08 21:35:25 +0000

    whitespace fixes and some comments of the default route
    git-svn-id: https://svn.liip.ch/repos/public/okapi/skel/branches/symfony2@12974 99f7f646-bdd5-0310-89f9-ddcb10b10bf3

--- conf/commandmap.php
+++ conf/commandmap.php
@@ -1,11 +1,16 @@
 <?php
 
-$routing->route('default', '*',
-array(
+// route name then url with :params, * is the catch-all
+$routing->route('default', '*', 
+    // options
+    array(
         'command' => 'default',
         'method' => 'index',
-        'view' => array ('xsl' => 'default.xsl')
-    )
+        'view' => array('xsl' => 'default.xsl')
+    ),
+    // default values for url params
+    array(),
+    // requirements (regex) for url params
+    array(),
 );
-
 
